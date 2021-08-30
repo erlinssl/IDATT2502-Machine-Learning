f65efd2d353dbea388a69d8af9aceafe3625b0cf
erlinssl <erlinssl@stud.ntnu.no>
2021-08-30 23:22:03 +0200

    Exercise 1 framework

---
+++ Exercise 1/testing.py
@@ -0,0 +1,7 @@
+import torch
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+x_train = torch.tensor([1.0, 1.5, 2.0, 3.0, 4.0, 5.0, 6.0]) # .reshape(-1, 1)  # x_train = [[1], [1.5], [2], [3], [4], [5], [6]]
+print(x_train)
