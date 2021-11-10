7aad68b7081454ee053d5ecf1b901ca5be008f66
erlinssl <erlinssl@stud.ntnu.no>
2021-11-10 23:01:50 +0100

    Attempted fitting of model, unsuccessful.

--- Exercises/Exercise 8 - Reinforcement Learning/dqn-agent.py
+++ Exercises/Exercise 8 - Reinforcement Learning/dqn-agent.py
@@ -15,8 +15,8 @@ TRAIN_PATH = os.path.join(os.path.dirname(__file__), 'trained_model.pt')
 EPS_START = 0.9
 EPS_END = 0.05
 EPS_DECAY = 1.25
-BATCH_SIZE = 64
-CAPACITY = BATCH_SIZE * 16
+BATCH_SIZE = 128
+CAPACITY = BATCH_SIZE * 256
 
 
 class ReplayMemory:
@@ -40,18 +40,14 @@ class DQNAgent(nn.Module):
     def __init__(self, action_space, in_channels=4):
         super(DQNAgent, self).__init__()
         self.memory = ReplayMemory(CAPACITY)
-        self.dense = nn.Linear(in_channels, 32)
-        self.dense2 = nn.Linear(32, 64)
-        self.dense3 = nn.Linear(64, 32)
-        self.dense4 = nn.Linear(32, action_space)
+        self.dense = nn.Linear(in_channels, 64)
+        self.dense2 = nn.Linear(64, 256)
+        self.dense3 = nn.Linear(256, 64)
+        self.dense4 = nn.Linear(64, action_space)
         self.dense21 = nn.Linear(in_channels, 256)
         self.dense22 = nn.Linear(256, action_space)
 
     def forward(self, x):
-        # x = F.relu(self.dense(x))
-        # x = F.relu(self.dense2(x))
-        # x = F.relu(self.dense3(x))
-        # x = F.relu(self.dense4(x))
         x = F.relu(self.dense21(x))
         x = F.relu(self.dense22(x))
         return x
@@ -67,8 +63,12 @@ class DQNAgent(nn.Module):
         batch_reward = torch.cat(batch_reward)
         batch_next_state = torch.cat(batch_next_state)
 
-        current_q_values = model(batch_current_state).gather(1, batch_action)
-        max_next_q_values = model(batch_next_state).detach().max(1)[0]
+        # print(batch_current_state.shape)
+        # print(batch_action.shape)
+        # print(model(batch_current_state).gather(1, batch_action).shape)
+        # time.sleep(5)
+        current_q_values = self(batch_current_state).gather(1, batch_action)
+        max_next_q_values = self(batch_next_state).detach().max(1)[0]
         expected_q_values = batch_reward + (0.8 * max_next_q_values)  # 0.8 = discount rate
 
         loss = F.smooth_l1_loss(current_q_values.squeeze(), expected_q_values.squeeze())
@@ -81,30 +81,32 @@ class DQNAgent(nn.Module):
         if not os.path.exists(os.path.dirname(TRAIN_PATH)):
             os.makedirs(os.path.dirname(TRAIN_PATH))
 
-        torch.save(model.state_dict(), TRAIN_PATH)
+        torch.save(self.state_dict(), TRAIN_PATH)
         print("Model saved!")
 
     def load(self):
         self.load_state_dict(torch.load(TRAIN_PATH))
         print("Model loaded!")
-        model.eval()
+        self.eval()
 
 
 env = gym.make('CartPole-v0')
 
 model = DQNAgent(env.action_space.n)
 steps_done = 0
+expl_steps = 0
 optimizer = optim.RMSprop(model.parameters())
 episode_durations = []
 
 
-def exploration_rate(n: int, min_rate=0.1) -> float:
+def exploration_rate(n: int, min_rate=0.01) -> float:
     return max(min_rate, 1 / EPS_DECAY ** n)
     # max(min_rate, min(1, 1.0 + np.log10(n + 1 / 25)))
 
 
 def select_action(state):
     global steps_done
+    global expl_steps
     rand = round(np.random.random(), 1)
     expl = float(exploration_rate(steps_done))
     # print(steps_done, ": ", rand, "<", expl)
@@ -114,17 +116,19 @@ def select_action(state):
         with torch.no_grad():
             return model(state).type(torch.FloatTensor).data.max(1)[1].view(1, 1)
     else:
+        expl_steps += 1
         # print("eplox:", rand, "<", expl, (rand < expl))
         return env.action_space.sample()
 
 
 train = True
 if train:
-    for i_episode in range(1000):
+    print("Training!")
+    for i_episode in range(250):
         current_state = env.reset()
-
+        expl_steps = 0
         for t in range(200):
-            env.render()
+            # env.render()
             action = select_action(torch.FloatTensor([current_state]))
             # time.sleep(1)
             if isinstance(action, int):
@@ -149,30 +153,31 @@ if train:
 
             if done:
                 steps_done += 1
-                print("Episode {epnum: <3} exited after {stepnum: <3} steps".format(
+                print("Episode {epnum: <3} exited after {stepnum: <3} steps, {expl: <3} explorational".format(
                     epnum=i_episode,
                     stepnum=t,
+                    expl=expl_steps,
                 ))
                 break
+    model.save()
+    torch.save(optimizer.state_dict(), OPTIM_PATH)
 else:
+    print("Testing!")
     model.load()
+    model.eval()
+    optimizer.load_state_dict(torch.load(OPTIM_PATH))
     for i_episode in range(10):
         current_state = env.reset()
         for t in range(200):
             env.render()
             action = select_action(torch.FloatTensor([current_state]))
-            if action is int:
-                next_state, reward, done, _ = env.step(action)
-            else:
-                next_state, reward, done, _ = env.step(action.item())
+            next_state, reward, done, _ = env.step(action)
 
             if done and t < 199:
                 reward = -1
 
             model.memory.append((torch.FloatTensor([current_state]), action,
                            torch.FloatTensor([next_state]), torch.FloatTensor([reward])))
-
-            current_state = next_state
 
             if done:
                 print("Episode {epnum: <3} exited after {stepnum: <3} steps".format(
@@ -181,4 +186,10 @@ else:
                 ))
                 break
 
-# model.save()
+# print("Model's state_dict:")
+# for params in model.state_dict():
+#     print(params, '\t', model.state_dict()[params].size())
+
+# print("Optimizer's state_dict:")
+# for var_name in optimizer.state_dict():
+#     print(var_name, "\t", optimizer.state_dict()[var_name])
