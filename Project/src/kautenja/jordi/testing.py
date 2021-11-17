0f17561561c63e3e130fbc19f179848c44b19bfc
erlinssl <erlinssl@stud.ntnu.no>
2021-11-17 22:11:32 +0100

    New screepcaps. Redundant file cleanup

--- Project/src/kautenja/jordi/testing.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-img = cv2.imread('python_V2hu73FElf.png')
-cropped = img[47:209, 95:176, 0] * 0.299 + img[47:209, 95:176, 1] * 0.587 + img[47:209, 95:176, 2] * 0.114
-resized_screen = cv2.resize(img, (84, 110), interpolation=cv2.INTER_AREA)
-
